728d65610cdf61c4c59240bc1e603a56adc59cca
崔永亮 <702625325@qq.com>
2024-05-23 09:59:23 +0800

    fix up get imgs from t2i and i2i

--- get_image.py
+++ get_image.py
@@ -3,26 +3,18 @@ import glob
 from pathlib import Path
 from PIL import Image
 from modules import shared, paths, script_callbacks
-import time
+
 
 def get_img_from_txt2img():
     talker_path = str(paths.script_path) + "/outputs"
-    dateNow = time.strftime("%Y-%m-%d", time.localtime())
-    if os.path.exists(talker_path + "/txt2img-images/" +dateNow):
-        dir = talker_path + "/txt2img-images/" + dateNow
-    else:
-        dir = talker_path + "/txt2img-images"
-    files = glob.glob(os.path.join(dir, '*'))
+    dir = talker_path + "/txt2img-images"
+    files = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(dir)) for f in fn]
     latest_file = max(files, key=os.path.getmtime)
     return Image.open(latest_file)
 
 def get_img_from_img2img(x):
     talker_path = str(paths.script_path) + "/outputs"
-    dateNow = time.strftime("%Y-%m-%d", time.localtime())
-    if os.path.exists(talker_path + "/img2img-images/" +dateNow):
-        dir = talker_path + "/img2img-images/" + dateNow
-    else:
-        dir = talker_path + "/img2img-images"
-    files = glob.glob(os.path.join(dir, '*'))
+    dir = talker_path + "/img2img-images"
+    files = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(dir)) for f in fn]
     latest_file = max(files, key=os.path.getmtime)
     return Image.open(latest_file)
